f662d3b9ca20d43cad8cfa01b0c53363421f075b
Łukasz Reszke <lukaszreszke93@gmail.com>
2024-11-03 14:38:52 +0100

    Book scooter in advance TDD

---
+++ DrivelloTests/BookScooterInAdvanceTests.cs
@@ -0,0 +1,16 @@
+namespace DrivelloTests;
+
+public class BookScooterInAdvanceTests
+{
+    [Fact]
+    public void booking_scooter_in_advance()
+    {
+        
+    }
+
+    [Fact]
+    public void booking_scooter_in_advance_is_not_possible_if_it_is_not_available()
+    {
+        
+    }
+}
